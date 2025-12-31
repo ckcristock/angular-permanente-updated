9ef5064d08fba902ce80661000bff4716140eee2
Cristian Montoya <86530903+ckcristock@users.noreply.github.com>
2025-12-31 06:08:54 -0500

    create auth.service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,10 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+
+interface LoginResponse {
+  accessToken: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly API_URL = 'http://localhost:3000'; // ajusta luego
+  private readonly TOKEN_KEY = 'access_token';
 
-constructor() { }
+  constructor(private http: HttpClient) {}
 
+  login(email: string, password: string) {
+    return this.http
+      .post<LoginResponse>(`${this.API_URL}/auth/login`, {
+        email,
+        password,
+      })
+      .pipe(
+        tap((response) => {
+          this.setToken(response.accessToken);
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.TOKEN_KEY);
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  private setToken(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
 }
