e0a4de9f5d01531b15d9578ae72c98f66f3c4916
Cristian Montoya <86530903+ckcristock@users.noreply.github.com>
2025-12-31 05:33:34 -0500

    create login route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent),
+  },
+  //   {
+  //     path: '',
+  //     loadComponent: () => import('./pages/layout/layout.component').then((m) => m.LayoutComponent),
+  //     children: [
+  //       {
+  //         path: 'dashboard',
+  //         loadComponent: () =>
+  //           import('./pages/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  //       },
+  //     ],
+  //   },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
