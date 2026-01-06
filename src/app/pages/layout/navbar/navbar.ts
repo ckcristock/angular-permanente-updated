17a17c90efd9451644e2e0636ee107121e26a6ac
Cristian Montoya <86530903+ckcristock@users.noreply.github.com>
2026-01-05 21:49:15 -0500

    create components topbar, navbar and footer without material

--- src/app/pages/layout/navbar/navbar.ts
+++ src/app/pages/layout/navbar/navbar.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MenuItemsService, MenuItem } from '../../../core/services/menuItems.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  menuItems: MenuItem[];
 
+  constructor(private menuService: MenuItemsService) {
+    this.menuItems = this.menuService.getMenu();
+  }
 }
